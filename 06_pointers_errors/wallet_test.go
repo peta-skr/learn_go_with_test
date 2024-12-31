76b78c1ba50c1f509d2dfc0edc8c8b127f9cd7e2
peta-skr <atu.cho.33@gmail.com>
2024-12-31 10:46:36 +0900

    complete 06_pointers_errors

---
+++ 06_pointers_errors/wallet_test.go
@@ -0,0 +1,54 @@
+package chapter06
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		} 
+	}
+
+	assertError := func(t *testing.T, got error, want error) {
+		t.Helper()
+		if got == nil {
+			t.Errorf("wanted an error but didn't get one")
+		}
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+		assertError(t, err, ErrInsufficientFunds)
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}	
