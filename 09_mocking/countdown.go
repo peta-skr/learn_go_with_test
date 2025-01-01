322b2d408b1b829e8d068ecebb249f343694f87e
peta-skr <atu.cho.33@gmail.com>
2025-01-01 11:11:37 +0900

    complete 09_mocking

--- 09_mocking/countdown.go
+++ 09_mocking/countdown.go
@@ -22,11 +22,44 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-type DefaultSleeper struct {
+// type DefaultSleeper struct {
+// }
+
+// func (d *DefaultSleeper) Sleep() {
+// 	time.Sleep(1 *time.Second)
+// }
+
+type CountdownOperationsSpy struct {
+	Calls []string
 }
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 *time.Second)
+const write = "write"
+const sleep = "sleep"
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
@@ -41,6 +74,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
